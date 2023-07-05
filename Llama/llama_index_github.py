ae44b0d30290656f135f1ca982110bd49e32599f
skelley <stevek@virtustate.com>
2023-07-05 09:05:40 -0500

    new computer

--- Llama/llama_index_github.py
+++ Llama/llama_index_github.py
@@ -2,9 +2,11 @@ import pickle
 import os
 from llama_index import GPTSimpleVectorIndex
 from llama_index import download_loader
-from llama_index.readers.llamahub_modules.github_repo import GithubClient, GithubRepositoryReader
+# from llama_index.readers.llamahub_modules.github_repo import GithubClient, GithubRepositoryReader
+from llama_index import GPTQdrantIndex
+import qdrant_client
 import sys
-sys.path.insert(0, '../')
+sys.path.insert(0, './')
 import local_secrets as secrets
 
 os.environ['OPENAI_API_KEY'] = secrets.techstyle_openai_key
@@ -13,8 +15,8 @@ os.environ['GITHUB_TOKEN'] = secrets.ssk_github_token
 download_loader("GithubRepositoryReader")
 
 docs = None
-if os.path.exists("docs.pkl"):
-    with open("docs.pkl", "rb") as f:
+if os.path.exists("github_llama_index.pkl"):
+    with open("github_llama_index.pkl", "rb") as f:
         docs = pickle.load(f)
 
 if docs is None:
@@ -23,17 +25,17 @@ if docs is None:
         github_client,
         owner =                  "jerryjliu",
         repo =                   "llama_index",
-        filter_directories =     (["llama_index", "docs"], GithubRepositoryReader.FilterType.INCLUDE),
-        filter_file_extensions = ([".py"], GithubRepositoryReader.FilterType.INCLUDE),
+        filter_directories =     (['llama_index', 'docs', 'examples'], GithubRepositoryReader.FilterType.INCLUDE),
+        filter_file_extensions = (['.py', '.md', '.ipynb'], GithubRepositoryReader.FilterType.INCLUDE),
         verbose =                True,
         concurrent_requests =    10,
     )
-
     docs = loader.load_data(branch="main")
-
-    with open("docs.pkl", "wb") as f:
+    with open("github_llama_index.pkl", "wb") as f:
         pickle.dump(docs, f)
 
-index = GPTSimpleVectorIndex.from_documents(docs)
+client = qdrant_client.QdrantClient(url='http://localhost:6333')
+index = GPTQdrantIndex.from_documents(docs, client=client, collection_name='github_llama_index', disallowd_special=())
 
-print(index.query("Explain each LlamaIndex class?"))
+#index = GPTSimpleVectorIndex.from_documents(docs)
+#print(index.query("Explain each LlamaIndex class?"))
